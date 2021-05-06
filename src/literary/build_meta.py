81b9e1ab5622bd43e70695e533cc6f04958b798f
Angus Hollands <goosey15@gmail.com>
2021-05-06 08:11:40 +0100

    WIP: print path to overlay

--- src/literary/build_meta.py
+++ src/literary/build_meta.py
@@ -3,9 +3,25 @@ from setuptools.build_meta import (
     build_wheel as orig_build_wheel,
 )
 from literary.commands.build import LiteraryBuildApp
+import shutil
+import pathlib
+import os
+
+
+def _find_jupyter_path():
+    jupyter_path = pathlib.Path(shutil.which("jupyter"))
+    return jupyter_path.parent.parent / "share" / "jupyter"
+
+
+def _patch_jupyter_path():
+    paths = os.environ.get('JUPYTER_PATH', '').split(os.path.sep)
+    paths = [str(_find_jupyter_path())] + paths
+    os.environ['JUPYTER_PATH'] = os.path.sep.join(paths)
 
 
 def _build_literary():
+    _patch_jupyter_path()
+    
     LiteraryBuildApp.launch_instance(argv=[])
 
 
